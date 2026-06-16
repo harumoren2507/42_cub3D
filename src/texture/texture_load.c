107c7e084918e2546b7009fdc1c696ed9274488c
retoriya <retoriya@student.42tokyo.jp>
2026-06-17 06:37:37 +0900

    壁テクスチャのXPMファイル読み込み処理を追加

---
+++ src/texture/texture_load.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   texture_load.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: retoriya <retoriya@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/14 12:51:39 by retoriya          #+#    #+#             */
+/*   Updated: 2026/06/17 06:37:37 by retoriya         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../inc/cub3D.h"
+
+int	load_texture(t_game *game, t_texture *tex, char *path)
+{
+	tex->img = mlx_xpm_file_to_image(game->screen.mlx,
+			path, &tex->width, &tex->height);
+	if (!tex->img)
+		return (set_error(game, "Failed to load texture (must be .xpm)"));
+	tex->addr = mlx_get_data_addr(tex->img,
+			&tex->bpp, &tex->line_len, &tex->endian);
+	return (0);
+}
+
+int	load_all_textures(t_game *game)
+{
+	if (load_texture(game, &game->textures.north, game->tex_paths[TEX_NO]))
+		return (-1);
+	if (load_texture(game, &game->textures.south, game->tex_paths[TEX_SO]))
+		return (-1);
+	if (load_texture(game, &game->textures.west, game->tex_paths[TEX_WE]))
+		return (-1);
+	if (load_texture(game, &game->textures.east, game->tex_paths[TEX_EA]))
+		return (-1);
+	free(game->tex_paths[TEX_NO]);
+	free(game->tex_paths[TEX_SO]);
+	free(game->tex_paths[TEX_WE]);
+	free(game->tex_paths[TEX_EA]);
+	game->tex_paths[TEX_NO] = NULL;
+	game->tex_paths[TEX_SO] = NULL;
+	game->tex_paths[TEX_WE] = NULL;
+	game->tex_paths[TEX_EA] = NULL;
+	return (0);
+}
