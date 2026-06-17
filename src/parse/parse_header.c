b9d03135551b7b8a2aadd40b0e91fcc15ceb164c
retoriya <retoriya@student.42tokyo.jp>
2026-06-18 08:51:58 +0900

    マップファイルのヘッダー情報（テクスチャパス・RGB色）のパース処理を実装

---
+++ src/parse/parse_header.c
@@ -0,0 +1,121 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_header.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: retoriya <retoriya@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/16 15:53:24 by retoriya          #+#    #+#             */
+/*   Updated: 2026/06/18 08:51:58 by retoriya         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../inc/cub3D.h"
+
+static int	parse_tex_path(t_game *game, char *line, char *id, int idx)
+{
+	char	*path;
+
+	if (ft_strncmp(line, id, ft_strlen(id)) != 0)
+		return (0);
+	if (game->tex_loaded[idx])
+		return (set_error(game, "Duplicate texture definition"));
+	path = ft_strtrim(line + ft_strlen(id), " \t");
+	if (!path || path[0] == '\0')
+	{
+		free(path);
+		return (set_error(game, "Empty texture path"));
+	}
+	game->tex_paths[idx] = path;
+	game->tex_loaded[idx] = 1;
+	return (1);
+}
+
+static int	parse_rgb(t_game *game, char *str, int *color)
+{
+	char	**parts;
+	int		r;
+	int		g;
+	int		b;
+
+	parts = ft_split(str, ',');
+	if (!parts || !parts[0] || !parts[1] || !parts[2] || parts[3])
+	{
+		free_parts(parts);
+		return (set_error(game, "Color must be R,G,B"));
+	}
+	if (!is_num_str(parts[0]) || !is_num_str(parts[1]) || !is_num_str(parts[2]))
+	{
+		free_parts(parts);
+		return (set_error(game, "Color values must be numeric"));
+	}
+	r = ft_atoi(parts[0]);
+	g = ft_atoi(parts[1]);
+	b = ft_atoi(parts[2]);
+	free_parts(parts);
+	if (r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255)
+		return (set_error(game, "Color value out of range (0-255)"));
+	*color = (r << 16) | (g << 8) | b;
+	return (1);
+}
+
+static int	parse_color(t_game *game, char *line, char id, int *flag)
+{
+	char	*rgb_str;
+	int		color;
+
+	if (line[0] != id || (line[1] != ' ' && line[1] != '\t'))
+		return (0);
+	if (*flag)
+		return (set_error(game, "Duplicate color definition"));
+	rgb_str = ft_strtrim(line + 1, " \t");
+	if (!rgb_str || rgb_str[0] == '\0')
+	{
+		free(rgb_str);
+		return (set_error(game, "Missing color values"));
+	}
+	if (parse_rgb(game, rgb_str, &color) < 0)
+	{
+		free(rgb_str);
+		return (-1);
+	}
+	free(rgb_str);
+	if (id == 'F')
+		game->floor_color = color;
+	else
+		game->ceil_color = color;
+	*flag = 1;
+	return (1);
+}
+
+static int	to_status(int ret)
+{
+	if (ret < 0)
+		return (-1);
+	return (0);
+}
+
+int	parse_header_line(t_game *game, char *line)
+{
+	int	ret;
+
+	ret = parse_tex_path(game, line, "NO ", TEX_NO);
+	if (ret != 0)
+		return (to_status(ret));
+	ret = parse_tex_path(game, line, "SO ", TEX_SO);
+	if (ret != 0)
+		return (to_status(ret));
+	ret = parse_tex_path(game, line, "WE ", TEX_WE);
+	if (ret != 0)
+		return (to_status(ret));
+	ret = parse_tex_path(game, line, "EA ", TEX_EA);
+	if (ret != 0)
+		return (to_status(ret));
+	ret = parse_color(game, line, 'F', &game->floor_loaded);
+	if (ret != 0)
+		return (to_status(ret));
+	ret = parse_color(game, line, 'C', &game->ceil_loaded);
+	if (ret != 0)
+		return (to_status(ret));
+	return (set_error(game, "Unknown header line"));
+}
