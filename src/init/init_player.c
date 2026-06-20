b8d102d5625b4cbcc0bbee8a9570b4566ce5c3b5
retoriya <retoriya@student.42tokyo.jp>
2026-06-20 23:59:25 +0900

    プレイヤーの初期位置と視線ベクトルの初期化処理を実装

---
+++ src/init/init_player.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_player.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: retoriya <retoriya@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/17 19:02:53 by retoriya          #+#    #+#             */
+/*   Updated: 2026/06/20 23:59:25 by retoriya         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../inc/cub3D.h"
+
+static void	set_player_angle(t_player *p, char dir)
+{
+	if (dir == 'N')
+		p->angle = -M_PI / 2;
+	else if (dir == 'S')
+		p->angle = M_PI / 2;
+	else if (dir == 'E')
+		p->angle = 0;
+	else
+		p->angle = M_PI;
+}
+
+void	find_player_pos(t_game *game)
+{
+	int		y;
+	int		x;
+	char	cell;
+
+	y = 0;
+	while (y < game->map_h)
+	{
+		x = 0;
+		while (x < game->map_w)
+		{
+			cell = game->map[y][x];
+			if (cell == 'N' || cell == 'S' || cell == 'E' || cell == 'W')
+			{
+				game->player.pos_x = x + 0.5;
+				game->player.pos_y = y + 0.5;
+				set_player_angle(&game->player, cell);
+				game->map[y][x] = '0';
+				return ;
+			}
+			x++;
+		}
+		y++;
+	}
+}
