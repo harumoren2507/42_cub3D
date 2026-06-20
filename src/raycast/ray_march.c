3eae485be677c9109ac18d17f42038bc85e1279e
retoriya <retoriya@student.42tokyo.jp>
2026-06-21 08:26:49 +0900

    DDAアルゴリズムによるレイマーチング処理の実装

---
+++ src/raycast/ray_march.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ray_march.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: retoriya <retoriya@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/19 08:50:19 by retoriya          #+#    #+#             */
+/*   Updated: 2026/06/21 08:26:49 by retoriya         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../inc/cub3D.h"
+
+void	ray_march_to_wall(t_ray *ray, char **map)
+{
+	while (map[ray->cell_y][ray->cell_x] != '1')
+	{
+		if (ray->side_x < ray->side_y)
+		{
+			ray->side_x += ray->delta_x;
+			ray->cell_x += ray->step_x;
+			ray->side = SIDE_EW;
+		}
+		else
+		{
+			ray->side_y += ray->delta_y;
+			ray->cell_y += ray->step_y;
+			ray->side = SIDE_NS;
+		}
+	}
+}
